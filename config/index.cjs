a187ec18cf60113b37ab28a33a244e2ee07e1f03
yaoyn996 <120158992+yaoyn996@users.noreply.github.com>
2023-06-24 14:33:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o2UvJ6AWvV2MQph2fL8-tS1cGu88',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'WsReDeUCwdzpQ1eS3MsSceSRCbTSuCzYMvrX1qfo9fg',
+      useTemplateId: 'S20ZQ7o-M5h_vhcoziTSiB2kcBG-buNmgkG4-oeKGJw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-09',
       festivals: [
